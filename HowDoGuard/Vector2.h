367f1127026d461acc7a1a5bd50daed8cf06d545
mattstruble <mattstruble@outlook.com>
2013-05-06 18:43:38 -0700

    Made Vector2.h pretty.

--- HowDoGuard/Vector2.h
+++ HowDoGuard/Vector2.h
@@ -13,29 +13,29 @@ public:
 		X,
 		Y;
 
-	Vector2(void);
-	Vector2(float x, float y);
+	Vector2( void );
+	Vector2( float x, float y );
 	Vector2( float both );
-	virtual ~Vector2(void);
+	virtual ~Vector2( void );
 
-	float distanceTo(Vector2 other);
+	float distanceTo( Vector2 other );
 
-	float directionToDeg(Vector2 other);
-	float directionToRad(Vector2 other);
+	float directionToDeg( Vector2 other );
+	float directionToRad( Vector2 other );
 
-	const Vector2 operator+(const Vector2 &rhs) const;
-	const Vector2 operator-(const Vector2 &rhs) const;
-	const Vector2 operator*(float value) const;
-	const Vector2 operator/(float value) const;
+	const Vector2 operator+( const Vector2 &rhs ) const;
+	const Vector2 operator-( const Vector2 &rhs ) const;
+	const Vector2 operator*( float value ) const;
+	const Vector2 operator/( float value ) const;
 
-	Vector2 &operator+=(const Vector2 &rhs);
-	Vector2 &operator-=(const Vector2 &rhs);
-	Vector2 &operator*=(float value);
-	Vector2 &operator/=(float value);
-	Vector2 &operator=(const Vector2 &rhs);
+	Vector2 &operator+=( const Vector2 &rhs );
+	Vector2 &operator-=( const Vector2 &rhs );
+	Vector2 &operator*=( float value );
+	Vector2 &operator/=( float value );
+	Vector2 &operator= ( const Vector2 &rhs );
 
-	bool operator==(const Vector2 &rhs) const;
-	bool operator!=(const Vector2 &rhs) const;
+	bool operator==( const Vector2 &rhs ) const;
+	bool operator!=( const Vector2 &rhs ) const;
 
 };
 

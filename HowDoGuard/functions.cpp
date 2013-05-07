c0d3d82ad072153883878ac6ec543e964acb12be
mattstruble <mattstruble@outlook.com>
2013-05-06 18:51:10 -0700

    Fixed some warnings within deg_to_rad and rad_to_deg.

--- HowDoGuard/functions.cpp
+++ HowDoGuard/functions.cpp
@@ -55,10 +55,10 @@ string basename( string fullPath )
 
 float deg_to_rad( float deg )
 {
-	return (deg * PI) / 180.0;
+	return (float)((deg * PI) / 180.0);
 }
 
 float rad_to_deg( float rad )
 {
-	return (rad * 180.0) / PI;
+	return (float)((rad * 180.0) / PI);
 }

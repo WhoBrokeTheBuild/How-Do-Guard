a752f8a6b0355bea5db4529dcabf8de3f3ee44e5
sdl-slane <sdl.slane@gmail.com>
2013-05-08 14:10:05 -0700

    Added half of GameInput.h

---
+++ HowDoGuard/GameInput.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef __GAME_INPUT_H__
+#define __GAME_INPUT_H__
+
+enum GameInput
+{
+	INVALID_GAME_INPUT = -1,
+	GAME_INPUT_UP,
+	GAME_INPUT_DOWN,
+	GAME_INPUT_LEFT,
+	GAME_INPUT_RIGHT,
+	NUM_GAME_INPUTS
+};
+
+#endif
